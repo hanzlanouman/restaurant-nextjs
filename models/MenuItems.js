210da3f55a0318cfa86cebab1905201ff0342e33
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2024-02-18 00:53:10 +0500

    Pandi ne quoatation dali hui thi Mongouri me

--- models/MenuItems.js
+++ models/MenuItems.js
@@ -1,23 +1,24 @@
-import {mongoose, Schema} from 'mongoose';
+import { mongoose, Schema } from 'mongoose';
 const MenuItemsSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    image: {
-        type: String,
-        required: true}
-      
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
 });
-const MenuItem = mongoose.models.MenuItem || mongoose.model('MenuItems', MenuItemsSchema);
+const MenuItem =
+  mongoose.models.MenuItem || mongoose.model('MenuItem', MenuItemsSchema);
 export default MenuItem;
 
 // mongoose.models.MenuItem ||
