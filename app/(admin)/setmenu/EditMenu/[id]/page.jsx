3d59d757d71dbaa92aca21fc89bdeb0d656d72c5
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2024-02-21 00:32:35 +0500

    Fixed Pandi.exe

--- app/(admin)/setmenu/EditMenu/[id]/page.jsx
+++ app/(admin)/setmenu/EditMenu/[id]/page.jsx
@@ -1,31 +1,26 @@
 import React from 'react';
 import EditItemsForm from '@/components/admin/EditItemsForm';
-
+import ConnectMongoDB from '@/lib/MongoDB';
+import MenuItem from '@/models/MenuItems';
 const fetchMenuItem = async (id) => {
   if (id) {
     try {
-      const res = await fetch(`/apis/MenuItems/${id}`, {
-        cache: 'no-cache',
-      });
-      if (!res.ok) {
-        throw new Error('Failed to fetch item');
-      }
-      return await res.json();
+      await ConnectMongoDB();
+      const menuItems = await MenuItem.findById(id);
+      console.log(menuItems);
+      return menuItems;
     } catch (error) {
       console.error('Error loading item:', error);
     }
   }
 };
-const EditMenu = async ({params}) => {
-  const {id} = params;
+const EditMenu = async ({ params }) => {
+  const { id } = params;
   const menuItem = await fetchMenuItem(id);
   console.log('Menu Item', menuItem);
   return (
     <div className='flex justify-center items-center bg-zinc-800 w-full py-4'>
-        <EditItemsForm
-         MenuItem={menuItem}
-        />
-
+      <EditItemsForm MenuItem={menuItem} />
     </div>
   );
 };
--- components/admin/editItemsForm.jsx
+++ components/admin/editItemsForm.jsx
@@ -1,9 +1,9 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-const EditItemsForm = ({ MenuItem =[] }) => {
-  const { title, description, image,price} = MenuItem;
+const EditItemsForm = ({ MenuItem }) => {
+  const { title, description, image, price } = MenuItem;
   const [newTitle, setTitle] = useState(title);
   const [newDescription, setDescription] = useState(description);
   const [newPrice, setPrice] = useState(price);
@@ -35,64 +35,65 @@ const EditItemsForm = ({ MenuItem =[] }) => {
   };
   return (
     <div className='flex justify-center items-center'>
-    <form onSubmit={handleSubmit}
-      className='space-y-4 text-white flex justify-center flex-col  w-full xl:min-w-[500px] min-w-[97.5vw] md:min-w-[400px] xl:items-start'
-    >
-      <div className='flex flex-col space-y-4 w-full  xl:max-w-[450px] rounded-xl bg-zinc-800 p-8 shadow-2xl '>
-        <div className='flex flex-col space-y-4'>
-          <h1 className='text-4xl font-semibold text-primary text-center xl:text-left md:text-left'>
-            Edit Items
-          </h1>
-          <div>
-            <label className='block mb-2'>Title</label>
-            <input
-              type='text'
-              placeholder='Product Title'
-              className='text-white bg-zinc-900 w-full p-2'
-              onChange={(e) => setTitle(e.target.value)}
-              value={newTitle}
-            />
+      <form
+        onSubmit={handleSubmit}
+        className='space-y-4 text-white flex justify-center flex-col  w-full xl:min-w-[500px] min-w-[97.5vw] md:min-w-[400px] xl:items-start'
+      >
+        <div className='flex flex-col space-y-4 w-full  xl:max-w-[450px] rounded-xl bg-zinc-800 p-8 shadow-2xl '>
+          <div className='flex flex-col space-y-4'>
+            <h1 className='text-4xl font-semibold text-primary text-center xl:text-left md:text-left'>
+              Edit Items
+            </h1>
+            <div>
+              <label className='block mb-2'>Title</label>
+              <input
+                type='text'
+                placeholder='Product Title'
+                className='text-white bg-zinc-900 w-full p-2'
+                onChange={(e) => setTitle(e.target.value)}
+                value={newTitle}
+              />
+            </div>
+            <div>
+              <label className='block mb-2'>Description</label>
+              <textarea
+                placeholder='Product Description'
+                className='text-white bg-zinc-900 w-full p-2'
+                onChange={(e) => setDescription(e.target.value)}
+                value={newDescription}
+              />
+            </div>
+            <div>
+              <label className='block mb-2'>Price</label>
+              <input
+                type='number'
+                placeholder='Product Price'
+                className='text-white bg-zinc-900 w-full p-2'
+                onChange={(e) => setPrice(e.target.value)}
+                value={newPrice}
+              />
+            </div>
+            <div>
+              <label className='block mb-2'>Image URL</label>
+              <input
+                type='text'
+                placeholder='Product Image URL'
+                className='text-white bg-zinc-900 w-full p-2'
+                onChange={(e) => setImage(e.target.value)}
+                value={newImage}
+              />
+            </div>
           </div>
-          <div>
-            <label className='block mb-2'>Description</label>
-            <textarea
-              placeholder='Product Description'
-              className='text-white bg-zinc-900 w-full p-2'
-              onChange={(e) => setDescription(e.target.value)}
-              value={newDescription}
-            />
-          </div>
-          <div>
-            <label className='block mb-2'>Price</label>
-            <input
-              type='number'
-              placeholder='Product Price'
-              className='text-white bg-zinc-900 w-full p-2'
-              onChange={(e) => setPrice(e.target.value)}
-              value={newPrice}
-            />
-          </div>
-          <div>
-            <label className='block mb-2'>Image URL</label>
-            <input
-              type='text'
-              placeholder='Product Image URL'
-              className='text-white bg-zinc-900 w-full p-2'
-              onChange={(e) => setImage(e.target.value)}
-              value={newImage}
-            />
+          <div className='flex justify-end items-end'>
+            <button
+              type='submit'
+              className='min-w-[120px] text-base bg-transparent border-2 border-primary hover:bg-primary hover:text-zinc-white hover:border-transparent p-2'
+            >
+              Add Product
+            </button>
           </div>
         </div>
-        <div className='flex justify-end items-end'>
-          <button
-            type='submit'
-            className='min-w-[120px] text-base bg-transparent border-2 border-primary hover:bg-primary hover:text-zinc-white hover:border-transparent p-2'
-          >
-            Add Product
-          </button>
-        </div>
-      </div>
-    </form>
+      </form>
     </div>
   );
 };
