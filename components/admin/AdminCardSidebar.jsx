d36a8232182dd83ce5227cbf6ae624a7c6edf8f4
Hanzla Nouman <64569364+HanzlaNouman@users.noreply.github.com>
2024-02-21 22:52:05 +0500

    Fixes

--- components/admin/AdminCardSidebar.jsx
+++ components/admin/AdminCardSidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/button';
 import { SignOutButton, currentUser } from '@clerk/nextjs';
 import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar';
 const AdminCardSidebar = async ({ displayStyles }) => {
